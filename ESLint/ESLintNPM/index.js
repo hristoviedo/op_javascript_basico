88f0967045f7cc851962d3b43365e7f6f69ef581
Hristo S. Oviedo <hristo.oviedohn@gmail.com>
2022-10-25 13:11:12 -0600

    feat(example): Agregar ejemplo de ESlint

---
+++ ESLint/ESLintNPM/index.js
@@ -0,0 +1,22 @@
+/**
+ * Para instalar eslint por NPM: npm -i -D eslint
+ * Para configurar las reglas en el archivo .eslintcr.json: npm init @eslint/config
+ * Scripts en el package.json:
+ *      "lint": "eslint .",
+        "lint-fix": "eslint --fix ."
+ * Para ver los errores y advertencias: npm run lint
+ * Para corregir conforme a las reglas: npm run lint-fix
+ */
+
+/*eslint-disable*/
+const nombre = 'juana';
+/* eslint-enable */
+
+/* eslint-disable-next-line no-console */
+console.log(nombre);
+const nombre1 = "juana";
+console.log(nombre1);
+console.log("nombre");
+
+
+
